a0a75b19426df6befaf12ef2bd39dd0bc42be152
Nguyễn Anh Bình <binhna1@fsoft.com.vn>
2022-09-24 12:13:09 +0700

    Add missing files

--- settings/database_settings.py
+++ settings/database_settings.py
@@ -1,4 +1,6 @@
+import logging
 import os
+
 import environ
 from django.conf import settings
 
@@ -7,10 +9,12 @@ env_file_name = env.get_value("ENV_FILE_NAME", default="envs/.local.env")
 env_file = os.path.join(settings.BASE_DIR, env_file_name)
 if os.path.isfile(env_file):
     env.read_env(env_file)
-print(f"Loading env file: {env_file}")
-DATABASES = {"default": env.db()}
+    print(f"Loading env file: {env_file}")
+    DATABASES = {"default": env.db()}
+    if bool(os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None)):
+        print("Using Cloud SQL Proxy")
+        DATABASES["default"]["HOST"] = "localhost"
+        DATABASES["default"]["PORT"] = 5432
 
-if bool(os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None)):
-    print("Using Cloud SQL Proxy")
-    DATABASES["default"]["HOST"] = "localhost"
-    DATABASES["default"]["PORT"] = 5432
+else:
+    logging.warning(f"Env file not found: {env_file}")
