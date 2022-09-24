95b6257a045c72a02ce2a127522dbad031219831
Nguyễn Anh Bình <binhna1@fsoft.com.vn>
2022-09-24 12:36:30 +0700

    TD-73: Warning when env file nto found

--- settings/database_settings.py
+++ settings/database_settings.py
@@ -7,14 +7,17 @@ from django.conf import settings
 env = environ.Env(DEBUG=(bool, True))
 env_file_name = env.get_value("ENV_FILE_NAME", default="envs/.local.env")
 env_file = os.path.join(settings.BASE_DIR, env_file_name)
+env_file = os.path.abspath(env_file)
+logging.info(f"Loading env file: {env_file}")
 if os.path.isfile(env_file):
     env.read_env(env_file)
-    print(f"Loading env file: {env_file}")
     DATABASES = {"default": env.db()}
     if bool(os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None)):
         print("Using Cloud SQL Proxy")
         DATABASES["default"]["HOST"] = "localhost"
         DATABASES["default"]["PORT"] = 5432
+    # add DATABASES to settings
+    settings.DATABASES = DATABASES
 
 else:
     logging.warning(f"Env file not found: {env_file}")
